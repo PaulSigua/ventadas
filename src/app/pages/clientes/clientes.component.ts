e96fc75bc7cf717b1157f76ee9257dacbb0bc8ec
PaulSigua <mateosigua2002@gmail.com>
2024-01-22 07:55:34 -0500

    cambios en la administracion de clientes

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { Cliente } from 'src/app/domain/cliente';
 import { ClienteService } from 'src/app/services/cliente.service';
 
@@ -11,24 +12,68 @@ export class ClientesComponent implements OnInit {
   clientes: any;
 
   client: Cliente = new Cliente();
+  mostrarBotonGuardar: boolean = true;
+  mostrarBotonActualizar: boolean = false;
+  mostrarBotonCancelar: boolean = false;
 
-  constructor(private clienteService: ClienteService) {
-
+  constructor(private clienteService: ClienteService,
+    private router: Router) {
+    window.scrollTo({
+      top: 0
+    })
   }
 
   ngOnInit(): void {
     this.clientes = this.clienteService.getClientes();
   }
 
+  cancelar() {
+    this.mostrarBotonGuardar = true;
+    this.mostrarBotonCancelar = false;
+    this.mostrarBotonActualizar = false;
+    this.client = {};
+  }
+
   guardar() {
-    this.clienteService.saveCliente(this.client).subscribe(data => {
+    if (this.client.codigo == null || this.client.dni == null || this.client.nombre == null || this.client.direccion == null) {
+      alert("Debe llenar todos los parametros")
+    } else {
+      this.clienteService.saveCliente(this.client).subscribe(data => {
+        console.log(data);
+        if (data.codigo == 1) {
+          this.client = new Cliente();
+          this.ngOnInit();
+        } else {
+          alert("Error al insertar" + data.message)
+        }
+      });
+    }
+  }
+
+  actualizar() {
+    this.clienteService.updateCliente(this.client).subscribe(data => {
       console.log(data);
-      if (data.codigo == 1) {
-        this.ngOnInit();
-      } else {
-        alert("Error al insertar" + data.message)
-      }
     });
+    this.client = {};
+    this.ngOnInit();
+    this.mostrarBotonCancelar = false;
+    this.mostrarBotonActualizar = false;
+    this.mostrarBotonGuardar = true;
+  }
+
+  editar(cliente: Cliente) {
+    this.client = { ...cliente };
+    this.mostrarBotonGuardar = false;
+    this.mostrarBotonCancelar = true;
+    this.mostrarBotonActualizar = true;
+  }
+
+  eliminar(cliente: number) {
+    this.clienteService.deleteCliente(cliente).subscribe(data => {
+      console.log(data);
+    });
+    console.log(cliente);
+    this.ngOnInit();
   }
 
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environments';
 import { Cliente } from '../domain/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,25 @@ export class ClienteService {
   constructor(private http: HttpClient) { }//Cuarta parte
 
   getClientes(){
-    let url = environment.WA_PATH+ '/clientes/list/';
+    let url = environment.WA_PATH+ '/clientes/list';
+    console.log(url);
     return this.http.get<any>(url);
   }
 
   saveCliente(cliente: Cliente){
-    let url = environment.WA_PATH + '/clientes'
+    let url = environment.WA_PATH + '/clientes';
     return this.http.post<any>(url, cliente);
+  }
+
+  updateCliente(cliente: Cliente){
+    let url = environment.WA_PATH + '/clientes'
+    return this.http.put<any>(url, cliente);
+  }
+
+  deleteCliente(cliente: number): Observable<any>{
+    let url = environment.WA_PATH + '/clientes?id=' + cliente;
+    console.log(url);
+    return this.http.delete<any>(url);
   }
 
 }
